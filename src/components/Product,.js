3d192fab37f51340758fb6c5f09eb0587f1395f2
danmainah <danielmainawaweru@gmail.com>
2022-01-17 17:58:01 +0300

    Create react app and add the 3 pages

---
+++ src/components/Product,.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class Product extends PureComponent {
+    render() {
+        return (
+            <div>
+                this is the product page
+            </div>
+        )
+    }
+}
