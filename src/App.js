fbb4521f89e37a5871bd20f8a0ca564e01402f2d
danmainah <danielmainawaweru@gmail.com>
2022-01-18 09:22:46 +0300

    Link stores and redux in App.js component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import React, { PureComponent } from 'react'
 import Category from './components/Category';
 import Product from './components/Product,';
 import Cart from './components/Cart';
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+import allReducers from "./reducers"
+
+let store = createStore(allReducers)
 
 export default class App extends PureComponent {
   render() {
     return (
-      <div>
-        <Category />
-        <Product />
-        <Cart />
+      <Provider store={store}>
+        <div>
+          <Category />
+          <Product />
+          <Cart />
       </div>
+      </Provider>
     )
   }
 }
