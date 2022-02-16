5b7f8d4d64f730bbdfb3ad69e296b3f73d8f5500
danmainah <danielmainawaweru@gmail.com>
2022-02-16 21:38:44 +0300

    Add components

---
+++ Cart.js
@@ -0,0 +1,8 @@
+import React from "react";
+import withContext from "../withContext";
+
+const Cart = props => {
+  // We can now access Context as props.context
+};
+
+export default withContext(Cart);
