2cc4bd647563ac34a518b2c960f03e76f0143177
danmainah <danielmainawaweru@gmail.com>
2022-02-17 10:57:09 +0300

    Add login method

--- src/App.js
+++ src/App.js
@@ -26,7 +26,37 @@ export default class App extends Component {
     user = user ? JSON.parse(user) : null;
     this.setState({ user });
   }
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      'http://localhost:3001/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
   
+    if(res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+  
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+
   render() {
     return (
       <Context.Provider
