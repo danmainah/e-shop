b5b9888389776ce513ea3a9e179b6a6cb932553a
danmainah <danielmainawaweru@gmail.com>
2022-03-28 21:58:52 +0300

    Reset the application

--- Cart.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import withContext from "../withContext";
-
-const Cart = props => {
-  // We can now access Context as props.context
-};
-
-export default withContext(Cart);
