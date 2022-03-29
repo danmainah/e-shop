8a60b7bdf5602ef297f3cc8dc575583e1f83728b
danmainah <danielmainawaweru@gmail.com>
2022-03-29 21:38:47 +0300

    Add login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 import AddProduct from './components/AddProduct';
 import Cart from './components/Cart';
@@ -19,6 +21,42 @@ export default class App extends Component {
     this.routerRef = React.createRef();
   }
 
+  componentDidMount() {
+    let user = localStorage.getItem("user");
+    user = user ? JSON.parse(user) : null;
+    this.setState({ user });
+  }
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      'http://localhost:3001/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+  
+    if(res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+  
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+  
   render() {
     return (
       <Context.Provider
@@ -88,13 +126,13 @@ export default class App extends Component {
                 )}
               </div>
             </nav>
-            <Switch>
+            <Routes>
               <Route exact path="/" component={ProductList} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/cart" component={Cart} />
               <Route exact path="/add-product" component={AddProduct} />
               <Route exact path="/products" component={ProductList} />
-            </Switch>
+            </Routes>
           </div>
         </Router>
       </Context.Provider>
